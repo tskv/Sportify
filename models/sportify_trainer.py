ebd21cc91d5eef0ade3f9f3d78a8ea3beae83b34
EnnyFRANS <150284277+EnnyFRANS@users.noreply.github.com>
2025-06-10 10:30:14 +0200

    basic model and views sportify trainer

---
+++ models/sportify_trainer.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class SportifyTrainer (models.Model):
+    _name = "sportify.trainer"
+    _description = "sport center management"
+
+    name = fields.Char(string="Name", required=True)
+    email = fields.Char(string="E-mail", required=True)
+    phone = fields.Char(string="Phone number", required=True)
+    speciality = fields.Char(string="Training Speciality", required=True)
