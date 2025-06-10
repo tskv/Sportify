a19cd339fc5ccf43d0297c46a7d7e3b47c54a179
Tatiana S <tatyana.skvortsova@gmail.com>
2025-06-10 11:09:41 +0200

    + field age

--- models/sportify_member.py
+++ models/sportify_member.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import date
 
 from odoo import models, fields, api
 
@@ -7,10 +8,24 @@ class SportifyMember(models.Model):
     _description = 'Member'
 
     active = fields.Boolean(string='Active', default=True)
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
     mail = fields.Char(string='Mail')
     phone = fields.Integer(string='Phone')
-    birth_date = fields.Date(string='Birth date')
-    #membership_id = fields.Many2one(comodel_name='', string='Membership')
+    birth_date = fields.Date(string='Birth date', required=True)
+    subscription_id = fields.Many2one(comodel_name='sportify.subscription', string='Subscription')
     inscription_date = fields.Date(string='Inscription date')
+    photo = fields.Binary()
+    age = fields.Integer(string='age', compute = '_compute_age')
+
+
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for record in self:
+            if record.birth_date:
+                today = date.today()
+                record.age = today.year - record.birth_date.year - ((today.month,today.day) < (record.birth_date.month, record.birth_date.day))
+            else:
+                record.age = 0
+
+
 
